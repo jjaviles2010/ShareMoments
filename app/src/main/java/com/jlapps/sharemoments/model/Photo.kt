ad95f029e957d7b5ede54fc07a7cf90c7e70ea87
Jose Javier Aviles Gomez <jjaviles2010@gmail.com>
2020-10-01 11:56:59 -0400

    Creating Photo model class.

---
+++ app/src/main/java/com/jlapps/sharemoments/model/Photo.kt
@@ -0,0 +1,20 @@
+package com.jlapps.sharemoments.model
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+@Entity(tableName = "photo")
+class Photo(
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "idPhoto") var idPhoto: Int = 0,
+    var title: String = "",
+    var filePath: String = "",
+    var fileFormat: String = "",
+    var height: Double = 0.0,
+    var width: Double = 0.0,
+    var photoDate: Long = 0,
+    var fileSize: Long = 0
+) : Parcelable
