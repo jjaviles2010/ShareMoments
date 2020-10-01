be7e2b5ab30b0e7c76de1327587aba03462d368a
Jose Javier Aviles Gomez <jjaviles2010@gmail.com>
2020-10-01 14:30:31 -0400

    Implementing photo's repository interface: https://trello.com/c/rnBNudvU/6-implement-repository-interface-and-class

---
+++ app/src/main/java/com/jlapps/sharemoments/repository/PhotoRepository.kt
@@ -0,0 +1,12 @@
+package com.jlapps.sharemoments.repository
+
+import androidx.lifecycle.LiveData
+import com.jlapps.sharemoments.model.Photo
+
+interface PhotoRepository {
+    suspend fun insertPhoto(photo: Photo) : Long
+
+    fun getPhotos() : LiveData<List<Photo>>
+
+    suspend fun updatePhoto(photo: Photo)
+}
